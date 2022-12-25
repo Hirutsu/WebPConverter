5dc1ac2ef47cdc5d4daae3f924c741587024e23f
burchuladze.levan <b_levan_a@mail.ru>
2022-12-26 00:03:21 +0400

    release 1.0

---
+++ ToWebpConverter/Program.cs
@@ -0,0 +1,45 @@
+﻿using ImageConverterToWebp;
+using System.Text.RegularExpressions;
+
+List<string> _folders;
+FileSystemWatcher _fileWather;
+List<FileSystemWatcher> _foldersWatcher = new();
+
+Console.Write("Введите полный путь к директории (без кавычек): ");
+string path = Console.ReadLine();
+Console.Write("Введите качество картинки 0 - 100: ");
+int imgQualityPercent = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+try
+{
+    _folders = Directory.GetDirectories(path).ToList();
+    _folders.Add(path);
+    foreach (var item in _folders)
+    {
+        _fileWather = new FileSystemWatcher(item);
+        _fileWather.Created += FileSystemWatcher_Created;
+        _fileWather.EnableRaisingEvents = true;
+        _foldersWatcher.Add(_fileWather);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Такого пути не существует: " + path);
+    Console.WriteLine(e.StackTrace);
+    Console.WriteLine(e.Message);
+    Console.WriteLine();
+}
+Console.ReadLine();
+
+void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+{
+    string fileExtension = new FileInfo(e.FullPath).Extension;
+
+    if (!Regex.IsMatch(fileExtension, @"\.png|\.jpg|\.jpeg", RegexOptions.IgnoreCase))
+        return;
+
+    Console.WriteLine($"Новый файл найден: {e.FullPath}");
+
+    WebPConverter.ConvertToWebP(e.FullPath, e.FullPath.Replace(fileExtension, ".webp"), imgQualityPercent);
+}
---
+++ ToWebpConverter/WebPConverter.cs
@@ -0,0 +1,41 @@
+﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using ImageProcessor.Plugins.WebP.Imaging.Formats;
+
+namespace ImageConverterToWebp
+{
+    public class WebPConverter
+    {
+        public static void ConvertToWebP(string inputPath, string outputPath, int imgQualityPercent)
+        {
+            ISupportedImageFormat webpFormat = new WebPFormat { Quality = imgQualityPercent };
+            ConvertImageToWebP(inputPath, outputPath, webpFormat);
+            Console.WriteLine();
+        }
+
+        private static void ConvertImageToWebP(string inputPath, string outputPath, ISupportedImageFormat outputImageFormat)
+        {
+            byte[] imageBytes;
+            Console.WriteLine($"Входной путь: {inputPath}");
+            Console.WriteLine($"Выходной путь: {outputPath}");
+            try
+            {
+                imageBytes = File.ReadAllBytes(inputPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Проблема с чтением файла");
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                return;
+            }
+
+            using MemoryStream memoryStream = new(imageBytes);
+            using ImageFactory imageFactory = new(preserveExifData: true);
+            var load = imageFactory.Load(memoryStream);
+            var format = load.Format(outputImageFormat);
+            var save = format.Save(outputPath);
+        }
+    }
+}
